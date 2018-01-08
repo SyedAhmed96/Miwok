be5cd350b108a86de3ca63714aeaafd4e436cef4
SyedAhmed96 <syedahmedullah96pk@gmail.com>
2018-01-09 03:19:59 +0500

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -0,0 +1,93 @@
+package com.example.android.miwok;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class PhrasesActivity extends AppCompatActivity {
+    private MediaPlayer mMediaPlayer;
+
+
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_list);
+
+        // Create a list of words
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("Where are you going?", "minto wuksus",
+                R.raw.phrase_where_are_you_going));
+        words.add(new Word("What is your name?", "tinnÓ™ oyaase'nÓ™",
+                R.raw.phrase_what_is_your_name));
+        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
+        words.add(new Word("How are you feeling?", "michÓ™ksÓ™s?", R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("Iâ€™m feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
+        words.add(new Word("Are you coming?", "Ó™Ó™nÓ™s'aa?", R.raw.phrase_are_you_coming));
+        words.add(new Word("Yes, Iâ€™m coming.", "hÓ™Ó™â€™ Ó™Ó™nÓ™m", R.raw.phrase_yes_im_coming));
+        words.add(new Word("Iâ€™m coming.", "Ó™Ó™nÓ™m", R.raw.phrase_im_coming));
+        words.add(new Word("Letâ€™s go.", "yoowutis", R.raw.phrase_lets_go));
+        words.add(new Word("Come here.", "Ó™nni'nem", R.raw.phrase_come_here));
+
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_phrases);
+        ListView listView = (ListView)findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                releaseMediaPlayer();
+
+                Word word = words.get(position);
+                mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, word.getAudioResourceId());
+                mMediaPlayer.start();
+
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+            }
+        });
+    }
+
+
+            protected void onStop(){
+            super.onStop();
+            //When the activity is stopped release the media player
+            releaseMediaPlayer();
+            }
+
+
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,98 @@
+package com.example.android.miwok;
+
+/**
+ * Created by whetzel on 8/12/16.
+ */
+public class Word {
+
+    /**
+     * Miwok translation for the word
+     */
+    private String mMiwokTranslation;
+
+    /**
+     * Default translation for the word
+     */
+    private String mDefaultTranslation;
+
+    /**
+     * Image resource ID for the word
+     */
+    private int mImageResourceId;
+
+    /**
+     * Constant value that represents no image was provided for this word
+     */
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    /** Audio resource ID for the word */
+    private int mAudioResourceId;
+
+
+    /**
+     * Constructor to create Word object with words only
+     *
+     * @param defaultTranslation
+     * @param miwokTranslation
+     */
+    public Word(String defaultTranslation, String miwokTranslation,int audioResourceId) {
+        mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
+        mAudioResourceId=audioResourceId;
+    }
+
+    /**
+     * Constructor to create Word object with words and images
+     *
+     * @param defaultTranslation
+     * @param miwokTranslation
+     */
+    public Word(String defaultTranslation, String miwokTranslation,int imageResourceId,int audioResourceId) {
+        mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+    /**
+     * Get Miwok translation of the default word
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    /**
+     * Get the Default translation of the miwok word
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Image Resource Id
+     *
+     * @return
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+
+    /**
+     * Return the audio resource ID of the word.
+     */
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+
+
+
+}
